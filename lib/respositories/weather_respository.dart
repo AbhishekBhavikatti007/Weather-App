138698c8952ae9395135ba6a62c5a4d5b717e47a
Abhishek B <abhirider893@gmail.com>
2024-11-09 07:50:29 +0530

    commit-1.0

---
+++ lib/respositories/weather_respository.dart
@@ -0,0 +1,32 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'package:weather_app_cubit/Models/customer_error.dart';
+import 'package:weather_app_cubit/Models/direct_gecoding.dart';
+import 'package:weather_app_cubit/Models/waether.dart';
+import 'package:weather_app_cubit/Services/weather_api_services.dart';
+import 'package:weather_app_cubit/expections/weather_exception.dart';
+
+class WeatherRespository {
+  final WeatherApiServices weatherApiServices;
+  WeatherRespository({
+    required this.weatherApiServices,
+  });
+
+  Future<Weather> featchCity(String city) async{
+    try {
+        final DirectGecoding directGecoding = await weatherApiServices.getDirectGecoding(city);
+        print(directGecoding);
+        final Weather tempWeather = await weatherApiServices.getWeather(directGecoding);
+        print(tempWeather);
+
+        final Weather weather = tempWeather.copyWith(name: directGecoding.name, country: directGecoding.country);
+        return weather;
+    }
+     on WeatherException catch (e) 
+     {
+      throw CustomerError(errMsg: e.message);
+    } 
+     catch (e) {
+      throw CustomerError(errMsg: e.toString());
+     }
+  }
+}
